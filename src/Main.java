f82dc7baf820ef26b0ff51845a4fab00ea8529bb
GulnazMm <[200~madamday1206@gmail.com~>
2022-04-30 20:14:58 +0300

    added files

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 3000;
+        int bonus = 20;
+
+
+        int mile =
+                price / bonus;
+
+        System.out.println("Ваши мили:" + mile);
+    }
+}
