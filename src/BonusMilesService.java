610041a35f02a73511a4766f1b69eb7ed1d705b7
GulnazMm <[200~madamday1206@gmail.com~>
2022-05-09 19:44:22 +0300

    added class

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,6 +1,6 @@
 public class BonusMilesService {
 
-    public int  calculate(int cost) {
+    public int calculate(int cost) {
         int bonus = 20;
         int miles = cost / bonus;
         return miles;
