bd5d3499ec4499f24bb4e6f4219746b9774ba997
GulnazMm <[200~madamday1206@gmail.com~>
2022-05-09 19:40:27 +0300

    added class

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int sum = 100;
-        int amount = 1000;
-        sum = amount + sum;
-
-        int bonus;
-        if (amount > 1000) {
-            bonus = (amount / 100);
-        } else {
-            bonus = 0;
-        }
-        sum = sum + bonus;
-
-        System.out.println("На счету:" + sum);
-        System.out.println("Бонусные рубли:" + bonus);
+        BonusMilesService service = new BonusMilesService();
+        int cost = 10_000;
+        int miles = service.calculate(cost);
+        System.out.println(miles);
 
     }
 }
